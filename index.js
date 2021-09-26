5dcb865b4b8252f139f3390be6b65f832423076a
BoberLob <75248620+Jaguar838@users.noreply.github.com>
2021-09-26 22:51:30 +0300

    init repo

---
+++ index.js
@@ -0,0 +1,51 @@
+console.log(`Hello Node.js v${process.versions.node}!`);
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: рефакторить
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      // ...
+      break;
+
+    case "get":
+      // ... id
+      break;
+
+    case "add":
+      // ... name email phone
+      break;
+
+    case "remove":
+      // ... id
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
+
+// # Получаем и выводим весь список контактов в виде таблицы (console.table)
+// node index.js --action list
+
+// # Получаем контакт по id
+// node index.js --action get --id 5
+
+// # Добавялем контакт
+// node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+
+// # Удаляем контакт
+// node index.js --action remove --id=3
